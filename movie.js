7fd468a98beb1c21230817785fe51a622d5faeed
crmoe <60358861+crmoe@users.noreply.github.com>
2020-05-02 00:44:04 -0600

    Update movie.js

--- movie.js
+++ movie.js
@@ -3,19 +3,14 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.USER_KEY, { useNewUrlParser: true } );
 
-
+mongoose.connect(process.env.DB_USER, { useNewUrlParser: true } );
 mongoose.connection.once('open', function(){
-    
     console.log('Movie conection has been made!');
-    
 }).on('error', function(error){
-    
     console.log('Error is: ', error);
 });
 mongoose.set('useCreateIndex', true);
-
 
 var MovieSchema = new Schema({
     title: String,
